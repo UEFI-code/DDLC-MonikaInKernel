fdff3b30d7ea75e9fac7148f46bb7043345d261f
UEFI <1092700291@qq.com>
2022-06-15 09:31:08 +0000

    Bug fix

--- MonikaDriver-Win10/MonikaBeep.asm
+++ MonikaDriver-Win10/MonikaBeep.asm
@@ -2,22 +2,28 @@
 
 BeepInit PROC PUBLIC
 
-push rax;
+cmp rcx, 0;
+ja process;
+mov rax, -1;
+ret;
+process:
+;;push rax;
 push rbx;
-push rdx;
-mov rax, 1193180000;
+;;push rdx;
+mov rax, 1193180;
 mov rbx, rcx;
 idiv rbx;
-mov rbx, rax; //save the result
+mov rbx, rax; save the result
 mov al, 182;
 out 67, al;
 mov al, bl;
 out 66, al;
 mov al, bh;
 out 66, al;
-pop rdx;
+;;pop rdx;
 pop rbx;
-pop rax;
+;;pop rax;
+mov rax, 0;
 ret;
 
 BeepInit ENDP
