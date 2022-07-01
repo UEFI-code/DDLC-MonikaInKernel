43b15d0da50fc9ff363fefe096a1aa8bc8510ce7
UEFI <1092700291@qq.com>
2022-07-01 12:04:55 +0000

    bug fix

--- MonikaUserMode/Tester.c
+++ MonikaUserMode/Tester.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<windows.h>
 
-#define RING3TO0_OBJ CTL_CODE(FILE_DEVICE_UNKNOWN, 0x910, METHOD_BUFFERED, FILE_WRITE_DATA)
+#define RING3TO0_OBJ CTL_CODE(FILE_DEVICE_UNKNOWN, 0x910, METHOD_IN_DIRECT, FILE_WRITE_DATA)
 #define RING0TO3_OBJ CTL_CODE(FILE_DEVICE_UNKNOWN, 0x911, METHOD_BUFFERED, FILE_READ_DATA)
 
 #define RING3_REQUIRE_TESTSTR 0
@@ -18,17 +18,16 @@ typedef struct
 
 int main()
 {
-	FILE *fp = fopen("a.txt", "rwb");
-	fseek(fp, -4, SEEK_END);
-	
+	//FILE *fp = fopen("a.txt", "rwb");
+	//fseek(fp, -4, SEEK_END);
 	MonikaObj a = {0};
-	a.type = 0x44;
+	a.type = 0x11;
 	strcpy(a.msg, "Monika Here!");
 	ULONG ret_code = 0;
 	HANDLE device = CreateFile("\\\\.\\Monika_Link", GENERIC_READ|GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
-	DeviceIoControl(device, RING3TO0_OBJ, &a, sizeof(MonikaObj), NULL, 0, &ret_code, 0);
+	DeviceIoControl(device, RING0TO3_OBJ, NULL, 0, &a, sizeof(MonikaObj), &ret_code, 0);
 	CloseHandle(device);
-	//printf("%s\n", a.msg);
+	printf("%s\n", a.msg);
 	printf("%u\n", ret_code);
 	system("pause");
 }
