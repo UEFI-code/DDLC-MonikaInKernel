7e097fa010874a76dc1ed72da19127bda22ab1d7
UEFI-code <zhuoshizhang@hotmail.com>
2024-08-26 14:10:18 +0000

    Add Features

--- MonikaUI/try.py
+++ MonikaUI/try.py
@@ -1,6 +1,10 @@
 import os
 from ctypes import *
 MonikaDLL = cdll.LoadLibrary(os.getcwd() +  '\\MonikaDLL.dll')
+
+print(f'Check SecureBoot: {MonikaDLL.check_secure_boot()}')
+print(f'Check Admin: {MonikaDLL.check_admin_privileges()}')
+
 msg = input("Enter your message: ").encode('utf-8')
 title = input("Enter your title: ").encode('utf-8')
 typ = int(input("Enter your type: "))
