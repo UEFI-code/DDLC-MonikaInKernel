5a11caa6c52b894a7178c334e1de79168e50860f
UEFI-code <zhuoshizhang@hotmail.com>
2024-11-07 18:00:48 +0900

    Add x86 WoW Support

---
+++ MonikaDLL/InjectX86Galgame.cpp
@@ -0,0 +1,45 @@
+#include "pch.h"
+
+extern "C"
+{
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <Windows.h>
+#include <tlhelp32.h>
+
+// x86 (Win32 on Win64) version of the Monika payload is not finished yet...
+
+DWORD GetProcessIdByName(const char* processName);
+DWORD GetMainThreadId(DWORD processId);
+LPVOID InjectShellcode(HANDLE hProcess, UINT8 *buf, UINT64 bufsize);
+HWND GetTargetWindowHandleByPID(DWORD processId);
+void DrawImageOnWindow(HWND hwnd, const char* imageFile);
+
+__declspec(dllexport) UINT8 injectX86Gal(char *targetEXE, const char *bmp_path)
+{
+    // First Get the PID of the target process
+    DWORD processId = GetProcessIdByName(targetEXE);
+    if (!processId)
+    {
+        printf("Target process \"%s\" not found. Exiting.\n", targetEXE);
+        return -1;
+    }
+    printf("Target process \"%s\" found with PID %lu\n", targetEXE, processId);
+
+    // Get Hwnd of the target process
+    HWND targetHwnd = GetTargetWindowHandleByPID(processId);
+    if (!targetHwnd)
+    {
+        printf("Failed to get target window handle\n");
+        return -1;
+    }
+
+    // Draw the image on the target window
+    DrawImageOnWindow(targetHwnd, bmp_path);
+
+    // Display MessageBoxA in the target process
+    MessageBoxA(targetHwnd, "JUST Monika", "JUST Monika", MB_OK | MB_ICONWARNING);
+}
+
+}
